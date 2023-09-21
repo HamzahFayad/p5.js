89df486392a1d2c9468f28289ffd4e5a340f9ef2
Hamzah Fayad <hamzah.fayad@glow-berlin.de>
2023-09-21 08:46:58 +0200

    movements + interactive

--- p5_filmposter/sketch.js
+++ p5_filmposter/sketch.js
@@ -1,6 +1,12 @@
 let y = 0;
 let ySpeed = 1;
 
+let r = 10;
+let rSpeed = 0.05;
+
+let s = -10;
+let sSpeed = 0.05;
+
 function setup() {
   createCanvas(500, 700);
   pixelDensity(2);
@@ -15,6 +21,22 @@ function draw() {
   }
   if (y < 0) {
     ySpeed *= -1;
+  }
+
+  r += rSpeed;
+  if (r > 10) {
+    rSpeed *= -1;
+  }
+  if (r < 0) {
+    rSpeed *= -1;
+  }
+
+  s += sSpeed;
+  if (r > -10) {
+    sSpeed *= -1;
+  }
+  if (r < 0) {
+    sSpeed *= -1;
   }
   background("#FF9B50");
 
@@ -34,30 +56,30 @@ function draw() {
   //SUN
   push();
   fill(255, 187, 92, 200);
-  circle(width / 2 - -y * 2, height / 2 - y, 100);
+  circle(width / 2 - -y * 2 + mouseX / 10, height / 2 - y, 100);
   pop();
 
   push();
   fill(198, 61, 47, 70);
-  rotate(10);
+  rotate(r);
   rect(0, height - height / 3 + y * 0.3, width * 2);
   pop();
 
   push();
   fill(198, 61, 47, 150);
-  rotate(-10);
+  rotate(s);
   rect(-150, height - height / 5 + y * 0.9, width * 2);
   pop();
 
   push();
   fill(226, 94, 62, 100);
-  rotate(5);
+  rotate(r / 2);
   rect(-150, height - height / 2 + y * 0.6, width * 2);
   pop();
 
   push();
   fill(198, 61, 47, 80);
-  rotate(-10);
+  rotate(s);
   rect(-150, height - 300 + y * 1.2, width * 2);
   pop();
 }
